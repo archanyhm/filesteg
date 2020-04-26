a395c6d84da72eb6e19d99fc6f9da8e8441c1176
Philipp Micke <archanyhm@gmail.com>
2020-04-26 21:58:29 +0200

    Supplied default values for the cmdparse structs to avoid undefined behaviour

--- lib/cmdparse.hpp
+++ lib/cmdparse.hpp
@@ -14,20 +14,20 @@ namespace cmdparse
         bool        required        = false;
         bool        takes_parameter = true;
 
-        char        shortOption;
-        std::string longOption;
+        char        shortOption     = 0;
+        std::string longOption      = "";
     };
 
     struct arg_match {
-        std::string option_name;
-        std::string caught_argument;
-        size_t      option_id;
-        bool        valid;
+        std::string option_name     = "";
+        std::string caught_argument = "";
+        size_t      option_id       = 0;
+        bool        valid           = false;
     };
 
     struct argv_meta {
-        std::string value;
-        bool        identified = false;
+        std::string value           = "";
+        bool        identified      = false;
     };
 
     std::vector<cmdparse::arg_match> parse(const int &argc, char *const argv[], const std::vector<cmdparse::arg> &possible_args);
